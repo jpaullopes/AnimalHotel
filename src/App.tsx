7f36acecf1db0d2da51f61a2831667b1e3b908f3
jpaullopes <jpaullopes27@gmail.com>
2025-11-27 09:46:23 -0300

    new project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Funciona!
+      </h1>
+    </div>
+  )
+}
+
+export default App
